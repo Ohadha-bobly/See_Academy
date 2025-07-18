c112921cfb78050577555401e7d3bb8374d05361
Geoffrey <geoffreyohadha@gmail.com>
2025-07-18 22:42:55 +0300

    We are adding the file even-odd checker

---
+++ even-odd.py
@@ -0,0 +1,9 @@
+def check_even_odd (number):
+    if number % 2 == 0:
+       print(number, "is Even ✔")
+    else:
+       print(number, "is Odd 😒")
+
+# Example usage
+check_even_odd(10)  
+check_even_odd(7)
